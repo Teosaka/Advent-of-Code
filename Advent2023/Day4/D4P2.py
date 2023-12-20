877a4d53a3f8a391c33e767ec4d3471c82a8f5dd
Teosaka <121055393+Teosaka@users.noreply.github.com>
2023-12-20 00:59:17 -0800

    Turns out which file I open in editor changes how I specify file path yay I love not knowing that till now anyways d5p2 is not done and I am stuck

--- Advent2023/Day4/D4P2.py
+++ Advent2023/Day4/D4P2.py
@@ -1,6 +1,7 @@
-# WIP
 with open ("Advent2023\Day4\Input.txt", "r") as file:
   total = 0
+  cardToCopies = {}
+  totalCards = {}
   for line in file.readlines():
     cardNumber = int(line[line.find(" ") + 1: line.find(":")])
     winning = line[line.find(":") + 2: line.find("|") - 1]
@@ -11,6 +12,21 @@ with open ("Advent2023\Day4\Input.txt", "r") as file:
     for num in haveNumbers:
       if num in winningNumbers:
         matching += 1
-    if matching >= 0:
-      total += 2 ** matching
+    cardToCopies.update({cardNumber: [i for i in range(cardNumber + 1, cardNumber + matching + 2)]})
+    totalCards.update({cardNumber: 1})
+  print(cardToCopies)
+  print(totalCards)
+  for key in totalCards.keys():
+    for i in range(totalCards.get(key)):
+      for copy in cardToCopies.get(key):
+        totalCards.update({copy: totalCards.get(copy) + 1})
+  print(totalCards)
+  for key in totalCards.keys():
+    total += totalCards.get(key)
   print(total)
+  #   cardToCopies.update({cardNumber: totalCards})
+  #   totalCards.append(cardNumber)
+  # for card in totalCards:
+  #   print(totalCards)
+  #   print(cardToCopies)
+  #   totalCards.append(copy for copy in cardToCopies.get(card) if len(cardToCopies.get(card)) > 0)
---
+++ Advent2023/Day5/D5P2.py
@@ -0,0 +1,78 @@
+def addMapping(string, map):
+  nums = string.split()
+  keyObj = int(nums[1].strip())
+  mapObj = int(nums[0].strip())
+  mapRange = int(nums[2].strip()) - 1
+  map.update({keyObj: [mapObj, keyObj + mapRange]})
+
+def checkMap(num, map):
+  for key in map.keys():
+    if key <= num <= map.get(key)[1]:
+      difference = num - key
+      return map.get(key)[0] + difference
+  return num
+
+def checkSeed(seed):
+  #print("seed:", seed)
+  soil = checkMap(seed, s2sMap)
+  #print("soil:", soil)
+  fertilizer = checkMap(soil, s2fMap)
+  #print("fert:", fertilizer)
+  water = checkMap(fertilizer, f2wMap)
+  #print("water:", water)
+  light = checkMap(water, w2lMap)
+  #print("light:", light)
+  temperature = checkMap(light, l2tMap)
+  #print("temp:", temperature)
+  humidity = checkMap(temperature, t2hMap)
+  #print("hum:", humidity)
+  location = checkMap(humidity, h2lMap)
+  #print("loc:", location)
+  return location
+
+with open ("Advent2023\Day5\Input.txt", "r") as file:
+  lowest = -1
+  seeds = []
+  lines = file.readlines()
+  s2s = lines.index("seed-to-soil map:\n")
+  s2sMap = {}
+  s2f = lines.index("soil-to-fertilizer map:\n")
+  s2fMap = {}
+  f2w = lines.index("fertilizer-to-water map:\n")
+  f2wMap = {}
+  w2l = lines.index("water-to-light map:\n")
+  w2lMap = {}
+  l2t = lines.index("light-to-temperature map:\n")
+  l2tMap = {}
+  t2h = lines.index("temperature-to-humidity map:\n")
+  t2hMap = {}
+  h2l = lines.index("humidity-to-location map:\n")
+  h2lMap = {}
+  for line in lines:
+    if lines.index(line)  == 0:
+      seedLine = line[line.find(":") + 1: -1]
+      for num in seedLine.split():
+          seeds.append(int(num.strip()))
+    elif (s2s < lines.index(line) < s2f - 1):
+      addMapping(line, s2sMap)
+    elif (s2f < lines.index(line) < f2w - 1):
+      addMapping(line, s2fMap)
+    elif (f2w < lines.index(line) < w2l - 1):
+      addMapping(line, f2wMap)
+    elif (w2l < lines.index(line) < l2t - 1):
+      addMapping(line, w2lMap)
+    elif (l2t < lines.index(line) < t2h - 1):
+      addMapping(line, l2tMap)
+    elif (t2h < lines.index(line) < h2l - 1):
+      addMapping(line, t2hMap)
+    elif (h2l < lines.index(line)):
+      addMapping(line, h2lMap)
+  while seeds:
+    start = seeds.pop(0)
+    end = seeds.pop(0) - 1
+    for seed in range(start, start + end):
+      seedLoc = checkSeed(seed)
+      if seedLoc < lowest or lowest < 0:
+        lowest = seedLoc
+  print(lowest)
+
